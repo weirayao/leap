f70170ec2d91779f68e3bf51a1ec8fbc65e2d251
Amber_Sun <32186215+YuewenSun@users.noreply.github.com>
2021-06-06 07:50:47 +0800

    Update train_nonlinear_vae.py

--- train_nonlinear_vae.py
+++ train_nonlinear_vae.py
@@ -28,26 +28,33 @@ def main(args):
         z_dim=args.z_dim, 
         lag=args.lag, 
         beta=args.beta, 
+        alpha=args.alpha, 
         gamma=args.gamma, 
-        lr=args.lr
+        lr=args.lr,
+        nonlinear_type=args.nonlinear_type
     )
 
-    trainer = pl.Trainer(default_root_dir="/home/cmu_wyao/checkpoints/nonlinear_vae",
+    trainer = pl.Trainer(default_root_dir="/home/yuewen/checkpoints/nonlinear_vae",
                          gpus=[0], 
                          val_check_interval=0.1,
-                         max_epochs=8,
-                         callbacks=[early_stop_callback]
+                         max_epochs=8
+                        #  ,
+                        #  callbacks=[early_stop_callback]
                          )
     # Train the model
     trainer.fit(model, train_loader, val_loader)
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description=__doc__)
+
     argparser.add_argument('-lag', default=1, type=int)
     argparser.add_argument('-z_dim', default=4, type=int)
     argparser.add_argument('-lr', default=5e-4, type=float)
-    argparser.add_argument('-beta', default=0.0005, type=float)
-    argparser.add_argument('-gamma', default=0.0075, type=float)
     argparser.add_argument('-input_dim', default=4, type=int)
+    argparser.add_argument('-beta', default=5e-5, type=float) # kld_normal
+    argparser.add_argument('-alpha', default=15, type=float) # coff
+    argparser.add_argument('-gamma', default=4, type=float) # recon_zt_
+    argparser.add_argument('-nonlinear_type', default='gaussian')
+
     args = argparser.parse_args()
     main(args)
