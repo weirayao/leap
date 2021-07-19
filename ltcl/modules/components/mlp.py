593a958d6c6a0e637bdef61ba60fa610588e40c7
weirayao <wyao1@andrew.cmu.edu>
2021-07-19 14:37:58 -0700

    Transition prior

---
+++ ltcl/modules/components/mlp.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+
+class NLayerLeakyMLP(nn.Module):
+
+    def __init__(self, in_features, out_features, num_layers, hidden_dim=64):
+        super().__init__()
+        layers = [ ]
+        for l in range(num_layers):
+            if l == 0:
+                layers.append(nn.Linear(in_features, hidden_dim))
+                layers.append(nn.LeakyReLU(0.2))
+            else:
+                layers.append(nn.Linear(hidden_dim, hidden_dim))
+                layers.append(nn.LeakyReLU(0.2))
+        layers.append(nn.Linear(hidden_dim, out_features))
+
+        self.net = nn.Sequential(*layers)
+    
+    def forward(self, x):
+        return self.net(x)
+
+class MLPEncoder(nn.Module):
+
+    def __init__(self, latent_size, num_layers=4, hidden_dim=64):
+        super().__init__()
+        self.net = NLayerLeakyMLP(in_features=latent_size, 
+                                  out_features=latent_size, 
+                                  num_layers=num_layers, 
+                                  hidden_dim=hidden_dim)
+    
+    def forward(self, x):
+        return self.net(x)
+
+class MLPDecoder(nn.Module):
+
+    def __init__(self, latent_size):
+        super().__init__()
+        # TODO: Do not use ground-truth decoder architecture 
+        self.net = nn.Sequential(nn.LeakyReLU(0.2),
+                                 nn.Linear(latent_size, latent_size),
+                                 nn.LeakyReLU(0.2),
+                                 nn.Linear(latent_size, latent_size))
+    
+    def forward(self, z):
+        return self.net(z)
+
+if __name__ == '__main__':
+    net = NLayerLeakyMLP(3,64,32,128)
+    print(net)
---
+++ ltcl/modules/components/transition.py
@@ -0,0 +1,81 @@
+"""Prior Network"""
+import torch
+import torch.nn as nn
+from ltcl.modules.components.mlp import NLayerLeakyMLP
+from ltcl.modules.components.graph import PropNet
+
+class LinearTransitionPrior(nn.Module):
+
+    def __init__(self, lags, latent_size, bias=False):
+        super().__init__()
+        self.init_hiddens = nn.Parameter(0.01 * torch.randn(latent_size, lags))
+        # out[:,:,0] = (x[:,:,0]@conv.weight[:,:,0].T)+(x[:,:,1]@conv.weight[:,:,1].T) 
+        # out[:,:,1] = (x[:,:,1]@conv.weight[:,:,0].T)+(x[:,:,2]@conv.weight[:,:,1].T)      
+        self.transition = nn.Conv1d(in_channels = latent_size, 
+                                    out_channels = latent_size, 
+                                    kernel_size = lags, 
+                                    bias=bias, 
+                                    padding_mode='zeros')
+    
+    def forward(self, x, mask=None):
+        # x: [BS, T, D]
+        x = x.permute(0,2,1).contiguous()
+        batch_size, length, _ = x.shape
+        init_hiddens = self.init_hiddens.repeat(batch_size, 1, 1)
+        # Pad learnable [BS, latent_size, lags] at the front
+        conv_in = torch.cat((init_hiddens, x), dim=-1)
+        conv_out = self.transition(conv_in)
+        # Drop the last bc no group-truth label
+        residuals = conv_out[...,:-1] - x
+        residuals = residuals.permute(0,2,1)
+        return residuals
+    
+
+class PNLTransitionPrior(nn.Module):
+
+    def __init__(
+        self, 
+        lags, 
+        latent_size, 
+        num_layers=3, 
+        hidden_dim=64):
+        super().__init__()
+        self.L = lags
+        self.init_hiddens = nn.Parameter(0.01 * torch.randn(lags, latent_size))       
+        self.f1 = NLayerLeakyMLP(in_features=lags*latent_size, 
+                                 out_features=latent_size, 
+                                 num_layers=num_layers, 
+                                 hidden_dim=hidden_dim)
+        
+        # Approximate the inverse of invertible function
+        self.f2 = NLayerLeakyMLP(in_features=latent_size, 
+                                 out_features=latent_size, 
+                                 num_layers=1, 
+                                 hidden_dim=hidden_dim)
+    
+    def forward(self, x, mask=None):
+        # x: [BS, T, D]
+        batch_size, length, _ = x.shape
+        init_hiddens = self.init_hiddens.repeat(batch_size, 1, 1)
+        if mask:
+            mask = mask.repeat(batch_size, 1)
+        # Pad learnable [BS, lags, latent_size] at the front
+        x_pad = torch.cat((init_hiddens, x), dim=1)
+        x_inv = self.f2(x)
+
+        residuals = [ ]
+        for t in range(length):
+            x_in = x_pad[:,t:t+self.L,:].view(batch_size, -1)
+            if mask:
+                x_in = x_in * mask
+            res = self.f1(x_in) - x_inv[:,t]
+            residuals.append(res)
+        residuals = torch.stack(residuals, dim=1)
+        return residuals
+
+#TODO: Markovian (1lag) Interaction Network Transition Prior
+class INTransitionPrior(nn.Module):
+
+    def __init__(self):
+        pass
+
